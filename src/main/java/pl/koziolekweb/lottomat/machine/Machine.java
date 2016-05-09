ff108e9b5cbf2e3d961f121c5a46e8be2d1ecc6b
Koziolek <bjkuczynski@gmail.com>
2016-05-09 21:27:19 +0200

    Simple abstract machine with set of parts

--- src/main/java/pl/koziolekweb/lottomat/machine/Machine.java
+++ src/main/java/pl/koziolekweb/lottomat/machine/Machine.java
@@ -1,25 +1,54 @@
 package pl.koziolekweb.lottomat.machine;
 
+import pl.koziolekweb.fun.Function0;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
  * TODO write JAVADOC!!!
  * User: koziolek
  */
-public abstract class Machine<T> {
+public abstract class Machine<IN, T> {
 
-	public T pick(Collection<T> input, int i) {
-
-		List<T> list = new ArrayList<>(input);
-		Collections.shuffle(list);
-
-		return
-				map(list.stream().limit(i).collect(Collectors.toList()));
+	public T pick() {
+		return getData()
+				.andThen(defensiveCopy())
+				.andThen(shuffle())
+				.andThen(drawN())
+				.andThen(mapToResult())
+				.apply(null);
 	}
 
-	protected abstract T map(List<T> drawn);
+	protected Function<Collection<IN>, List<IN>> defensiveCopy() {
+		return ArrayList::new;
+	}
+
+	protected Function<List<IN>, List<IN>> shuffle() {
+		return l -> {
+			Collections.shuffle(l);
+			return l;
+		};
+	}
+
+	protected BiFunction<List<IN>, Integer, List<IN>> draw() {
+		return (c, l) -> c.stream().limit(l).collect(Collectors.toList());
+	}
+
+	protected Function<List<IN>, List<IN>> drawN() {
+		return l -> draw().apply(l, limit());
+	}
+
+	protected abstract Function0<Collection<IN>> getData();
+
+	protected abstract Function<Collection<IN>, T> mapToResult();
+
+	protected abstract Integer limit();
+
+
 }
--- src/test/java/pl/koziolekweb/lottomat/machine/MachineTest.java
+++ src/test/java/pl/koziolekweb/lottomat/machine/MachineTest.java
@@ -3,9 +3,11 @@ package pl.koziolekweb.lottomat.machine;
 import com.google.common.collect.Lists;
 import org.fest.assertions.Assertions;
 import org.junit.Test;
+import pl.koziolekweb.fun.Function0;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
+import java.util.function.Function;
 
 /**
  * TODO write JAVADOC!!!
@@ -13,33 +15,60 @@ import java.util.List;
  */
 public class MachineTest {
 
-
-	Machine<Integer> sut = new Machine<Integer>() {
-		@Override
-		protected Integer map(List<Integer> drawn) {
-			return drawn.get(0);
-		}
-	};
-
 	@Test
 	public void shouldPickOneOfOne() throws Exception {
 
+		Machine<Integer, Integer> sut = new Machine<Integer, Integer>() {
 
-		Collection<Integer> input = Lists.newArrayList(1);
+			@Override
+			protected Function0<Collection<Integer>> getData() {
+				return () -> Lists.newArrayList(1);
+			}
 
-		Integer result = sut.pick(input, 1);
+			@Override
+			protected Function<Collection<Integer>, Integer> mapToResult() {
+				return c -> c.stream().findFirst().get();
+			}
+
+			@Override
+			protected Integer limit() {
+				return 1;
+			}
+		};
+
+
+		Integer result = sut.pick();
 
 		Assertions.assertThat(result).isEqualTo(1);
 	}
 
 	@Test
 	public void shouldRandomizeCopyOfInput() throws Exception {
-		List<Integer> input = Lists.newArrayList(1, 2, 3, 4, 5);
+		ArrayList<Integer> input = Lists.newArrayList(1, 2, 3, 4, 5);
 
-		Integer result = sut.pick(input, 1);
+		Machine<Integer, Integer> sut = new Machine<Integer, Integer>() {
+
+			@Override
+			protected Function0<Collection<Integer>> getData() {
+				return () -> input;
+			}
+
+			@Override
+			protected Function<Collection<Integer>, Integer> mapToResult() {
+				return c -> c.stream().findFirst().get();
+			}
+
+			@Override
+			protected Integer limit() {
+				return 1;
+			}
+		};
+
+
+		Integer result = sut.pick();
 
 		Assertions.assertThat(result).isIn(1, 2, 3, 4, 5);
-		Assertions.assertThat(input).containsSequence(1, 2, 3, 4, 5);
+		Assertions.assertThat(input). hasSize(5).containsExactly(1, 2, 3, 4, 5);
 
 	}
 }
