c015f6ff6d9414deb10c02904f24599323339222
Koziolek <bjkuczynski@gmail.com>
2016-05-10 12:17:44 +0200

    Add Ball and ResultOfDraw classes to our project

---
+++ src/main/java/pl/koziolekweb/lottomat/totolotek/Ball.java
@@ -0,0 +1,29 @@
+package pl.koziolekweb.lottomat.totolotek;
+
+import java.util.Objects;
+
+/**
+ * TODO write JAVADOC!!!
+ * User: koziolek
+ */
+public class Ball {
+
+	private final Integer value;
+
+	public Ball(Integer value) {
+		this.value = value;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Ball)) return false;
+		Ball ball = (Ball) o;
+		return Objects.equals(value, ball.value);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(value);
+	}
+}
---
+++ src/main/java/pl/koziolekweb/lottomat/totolotek/ResultsOfDraw.java
@@ -0,0 +1,18 @@
+package pl.koziolekweb.lottomat.totolotek;
+
+import java.util.Collection;
+
+/**
+ * TODO write JAVADOC!!!
+ * User: koziolek
+ */
+public class ResultsOfDraw {
+
+	private final Collection<Ball> balls;
+
+	public ResultsOfDraw(Collection<Ball> balls) {
+		this.balls = balls;
+	}
+
+
+}
