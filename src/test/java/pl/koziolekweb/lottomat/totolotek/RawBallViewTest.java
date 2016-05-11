dc0ceadb3e1391b67848e9f2b2b257c164139782
Koziolek <bjkuczynski@gmail.com>
2016-05-11 10:45:07 +0200

    Make RawBallViewTest red

--- src/test/java/pl/koziolekweb/lottomat/totolotek/RawBallViewTest.java
+++ src/test/java/pl/koziolekweb/lottomat/totolotek/RawBallViewTest.java
@@ -4,6 +4,13 @@ import org.fest.assertions.Assertions;
 import org.junit.Test;
 import pl.koziolekweb.lottomat.totolotek.Ball.RawBallView;
 
+import java.util.function.Consumer;
+
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 /**
  * TODO write JAVADOC!!!
  * User: koziolek
@@ -15,7 +22,12 @@ public class RawBallViewTest {
 		Ball ball = new Ball(1);
 		RawBallView sut = ball.new RawBallView();
 
-		sut.accept(i -> Assertions.assertThat(i).isEqualTo(1));
+		Consumer<Integer> consumer = mock(Consumer.class);
+		doAnswer(invocation -> Assertions.assertThat(invocation.getArguments()[0]).isEqualTo(1))
+				.when(consumer).accept(1);
+
+		sut.accept(consumer);
+		verify(consumer, atLeastOnce()).accept(1);
 
 	}
 }
